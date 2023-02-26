195a6016f158dae71d033d06120b9ee40c96dc16
Karston Kelly <bookelly11@gmail.com>
2023-02-26 22:42:21 +0000

    difference calculated

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -10,6 +10,16 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        int high = nums.get(0);
+        int low = nums.get(0);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums.get(i) > high){
+                high = nums.get(i);
+            }
+            if(nums.get(i) < low){
+                low = nums.get(i);
+            }
+        }
+        return high - low;
     }
 }
